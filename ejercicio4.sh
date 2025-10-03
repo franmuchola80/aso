de733033c13c7fc862aa02521d1f9d6a95c9bcac
franmuchola80 <framuccha@alu.edu.gva.es>
2025-10-03 09:47:14 +0200

    ejercicio4

---
+++ ejercicio4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Pedir primer número
+read -p "Introduce el primer número: " NUM1
+
+# Pedir segundo número
+read -p "Introduce el segundo número: " NUM2
+
+# Mostrar los números introducidos
+echo "Primer número: $NUM1"
+echo "Segundo número: $NUM2"
